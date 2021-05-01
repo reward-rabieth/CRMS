4d8a03643fb9062ce8b06cb62a4509284f7664e6
LouisNgatale <louisngatale@gmail.com>
2021-05-01 12:41:33 +0300

    login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,46 +1,45 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
+<div class="container-fluid login-page d-flex justify-content-center align-content-center">
+    <div class="row justify-content-center align-content-center">
+        <div class="col">
+            <div class="login-form container-fluid">
+                <div class="py-2 row justify-content-center align-content-center">
+                    <div class="col d-flex justify-content-center align-content-center">
+                        <img src="{{ url('/assets/images/logo.png') }}" alt="">
+                    </div>
+                </div>
+                <div class="row">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                        <div class="form-group justify-content-center align-items-center  row my-4">
+                            <div class="col-md-9">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" placeholder="Username" required autofocus>
 
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
+                                <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                        <div class="form-group justify-content-center align-items-center row my-4">
+                            <div class="col-md-9">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password"  placeholder="Password"  required autocomplete="current-password">
 
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
+                                <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
+                       {{-- <div class="form-group justify-content-start row">
+                            <div class="col-md-9 offset-md-2">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
@@ -49,14 +48,17 @@
                                     </label>
                                 </div>
                             </div>
-                        </div>
+                        </div>--}}
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                        <div class="form-group justify-content-center align-items-center row mb-3">
+                            <div class="col-md-9 ">
+                                <button type="submit" style="width: 100%" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
-
+                            </div>
+                        </div>
+                        <div class="justify-content-center align-items-center row">
+                            <div class="col-md-9 ">
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
@@ -64,10 +66,20 @@
                                 @endif
                             </div>
                         </div>
+
                     </form>
                 </div>
             </div>
         </div>
+        {{--<div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Login') }}</div>
+
+                <div class="card-body">
+
+                </div>
+            </div>
+        </div>--}}
     </div>
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,15 +14,17 @@
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ url('/css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
+{{--
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
@@ -76,8 +78,9 @@
                 </div>
             </div>
         </nav>
+--}}
 
-        <main class="py-4">
+        <main class="">
             @yield('content')
         </main>
     </div>
