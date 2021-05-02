85527086473558b7331944c95840ad9fbac95c49
LouisNgatale <louisngatale@gmail.com>
2021-05-02 12:11:00 +0300

    Side bar update

--- resources/views/admin/layout/admin.blade.php
+++ resources/views/admin/layout/admin.blade.php
@@ -1,9 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="side-bar">
+    <div class="side-bar pt-5">
 {{--        Navigation bar--}}
-        s
+        <ul>
+            <li>
+                <a href="">MANAGE STATIONS</a>
+            </li>
+            <li>
+                <a href="">MANAGE CRIMES</a>
+            </li>
+            <li>
+                <a href="">MANAGE POLICE</a>
+            </li>
+            <li>
+                <a href="">MANAGE CRIMINALS</a>
+            </li>
+            <li>
+                <a href="">MANAGE CASE</a>
+            </li>
+        </ul>
     </div>
     <main class="main">
         @yield('admin')
