9292fe3ccb8dd30e3f6d74046d56576f98bae11e
LouisNgatale <louisngatale@gmail.com>
2021-05-02 14:20:24 +0300

    Side bar update 2

--- resources/views/admin/layout/admin.blade.php
+++ resources/views/admin/layout/admin.blade.php
@@ -3,25 +3,27 @@
 @section('content')
     <div class="side-bar pt-5">
 {{--        Navigation bar--}}
-        <ul>
-            <li>
-                <a href="">MANAGE STATIONS</a>
-            </li>
-            <li>
-                <a href="">MANAGE CRIMES</a>
-            </li>
-            <li>
-                <a href="">MANAGE POLICE</a>
-            </li>
-            <li>
-                <a href="">MANAGE CRIMINALS</a>
-            </li>
-            <li>
-                <a href="">MANAGE CASE</a>
-            </li>
-        </ul>
+        <div class="category-wrapper">
+            <div class="nav-item collapse-link" onclick="expand(event)">MANAGE STATIONS</div>
+            <ul class="sub-category">
+                <li><a href="">Add</a></li>
+                <li><a href="">View</a></li>
+            </ul>
+        </div>
+        <div class="nav-item"><a href="">MANAGE CRIMES</a></div>
+        <div class="nav-item"><a href="">MANAGE POLICE</a></div>
+        <div class="nav-item"><a href="">MANAGE CRIMINALS</a></div>
+        <div class="nav-item"><a href="">MANAGE CASES</a></div>
     </div>
     <main class="main">
         @yield('admin')
     </main>
+
+    <script>
+        const expand = function (event){
+            let ul = event.target.nextElementSibling;
+            console.log(ul.classList.toggle('collapse'))
+        }
+
+    </script>
 @endsection
